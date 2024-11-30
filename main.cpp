ce8d46cb48706b6a74263a662154aef019de023a
Mayam Yehia <114694952+Maryamyehia03@users.noreply.github.com>
2024-11-30 19:45:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,88 +9,104 @@ using namespace std;
 #define iose ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
 
 
-int main() {
-    iose;
+int main() { iose;
+ 
+    fstream doctorstream("doctor.txt",ios::app | ios::binary);
+      Doctor d ;
 
-    fstream doctorstream("doctor.txt", ios::app | ios::binary);
-    Doctor d;
-
-    fstream appionmentstream("appointments.txt", ios::app);
+    fstream appionmentstream("appointments.txt",ios::app);
     Appointments app;
 
     short header = -1;
-    doctorstream.write((char *) &header, sizeof(header));
-    appionmentstream.write((char *) &header, sizeof(header));
+    doctorstream.write((char*)&header, sizeof(header));
+    appionmentstream.write((char*)&header, sizeof(header));
 
     int choice;
-    char more = 'y';
-    do {
-        cout << "-------------------------------------\n\n"
-             << "1)  Add New Doctor\n"
-             << "2)  Update Doctor\n"
-             << "3)  Delete Doctor\n"
-             << "4)  Display Doctor\n"
-             << "5)  Search by Doctor ID  \n"
-             << "6)  search by Doctor name\n"
-             << "7)  Add new Appointment\n"
-             << "8)  Update Appointment \n"
-             << "9)  Delete Appointment\n"
-             << "10) Display Appointment \n"
-             << "11) Search by Appointment ID \n"
-             << "12) Exit\n\n"
-             << "Please Enter Your Choice : ";
-        cin >> choice;
+    char more ='y';
+    do{
+    cout << "-------------------------------------\n\n"
+         << "1)  Add New Doctor\n"
+         << "2)  Update Doctor\n"
+         << "3)  Delete Doctor\n"
+         << "4)  Display Doctor\n"
+         << "5)  Search by Doctor ID  \n"
+         << "6)  search by Doctor name\n"
+         << "7)  Add new Appointment\n"
+         << "8)  Update Appointment \n"
+         << "9)  Delete Appointment\n"
+         << "10) Display Appointment \n"
+         << "11) Search by Appointment ID \n"
+         << "12) Exit\n"
+         << "Please Enter Your Choice :  " <<endl ;
+//        cout.flush();
+    cin >> choice;
 
-        cin.ignore();
+    cin.ignore();
 
-        if (choice == 1) {
+    if (choice == 1){
 
-            cout << "Enter Appointment Name:\n";
-            cin.getline(d.name, 30);  /// get name
+        cout << "Enter Doctor Name: "<<endl;
+        cin.getline(d.name, 30);  /// get name
 
-            cout << "Enter Doctor ID:\n";
-            cin.getline(d.id, 15); /// get id
+        cout << "Enter Doctor ID: " <<endl;
+        cin.getline(d.id, 15); /// get id
 
-            cout << "Enter Doctor Address:\n";
-            cin.getline(d.address, 30); /// get address
+        cout << "Enter Doctor Address: "<<endl;
+        cin.getline(d.address, 30); /// get address
 
-            d.writerecord(doctorstream);
-        } else if (choice == 2) {
-            d.update();
-        } else if (choice == 3) {
-            d.Delete();
+        d.writerecord(doctorstream);
+    }
+    else if (choice == 2){
+        d.update();
+    }
+    else if (choice == 3){
+        d.Delete();
 
-        } else if (choice == 4) {
-            d.Display();
-        } else if (choice == 5) {
-            d.searchbyid();
-        } else if (choice == 6) {
-            d.searchbyname();
-        } else if (choice == 7) {
+    }
+    else if (choice == 4){
+        d.Display();
+    }
+    else if (choice == 5){
+       d.searchbyid();
+    }
+    else if (choice == 6)
+    {
+       d.searchbyname();
+    }
+    else if (choice==7) {
 
-            cout << "Enter Doctor ID:\n";
-            cin.getline(app.id, 15);
+        cout << "Enter Appointment ID: "<<endl;
+        cin.getline(app.id, 15);
 
-            cout << "Enter Doctor ID:\n";
-            cin.getline(app.doctorID, 15);
+        cout << "Enter Doctor ID: "<<endl;
+        cin.getline(app.doctorID, 15);
 
-            cout << "Enter Appointment Date:\n";
-            cin.getline(app.date, 30);
+        cout << "Enter Appointment Date: "<<endl;
+        cin.getline(app.date, 30);
 
-            app.writerecord(doctorstream);
+        app.writerecord(doctorstream);
 
-        } else if (choice == 8) {
-            app.update();
-        } else if (choice == 9) {
-            app.Delete();
-        } else if (choice == 10) {
-            app.Display();
-        } else if (choice == 11) {
-            app.searchbyid();
-        } else
-            break;
+    }
+    else if (choice == 8)
+    {
+        app.update();
+    }
+    else if (choice == 9)
+    {
+        app.Delete();
+    }
+    else if (choice == 10)
+    {
+       app.Display();
+    }
+    else if (choice == 11)
+    {
+        app.searchbyid();
+    }
+    else
+        break ;
 
-        cout << "Do you want to enter more students?";
+        cout << "Do you want to enter more ?"<<endl;
         cin >> more;
         cin.ignore();
 
